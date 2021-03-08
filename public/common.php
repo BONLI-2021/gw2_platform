43fc9dcab3c3883b0f4bedccd6200cb063c5e93b
lyne007 <lyne007@163.com>
2021-03-08 10:51:23 +0800

    project init

---
+++ public/common.php
@@ -0,0 +1,39 @@
+<?php
+// 公共目录路径存放js、css、images等文件
+define('BASEURL','/');
+define('PUBLICURL',BASEURL.'public');
+// 服务器根
+define('ROOTPATH', rtrim(substr(BASEPATH, 0, strpos(BASEPATH, '/')), '/'));
+define('WWWROOTPATH', rtrim(FCPATH, '/'));
+
+// 初始化时区
+date_default_timezone_set('Asia/Shanghai');
+
+// 图片上传配置（本地）
+define('PIC_D_URL', 'http://'.$_SERVER['HTTP_HOST'].'/public/images/');//http默认图片访问地址
+define('PIC_O_URL', 'http://'.$_SERVER['HTTP_HOST'].'/public/upload/original/');//http图片原图访问地址
+define('PIC_T_URL', 'http://'.$_SERVER['HTTP_HOST'].'/public/upload/thumb/');//http缩略图访问地址
+
+define('PIC_O_PATH', '/public/upload/original/');//图片原图上传路径
+define('PIC_T_PATH', '/public/upload/thumb/');//缩略图上传路径
+
+// 企业logo上传配置(本地)
+define('UP_URL', 'http://'.$_SERVER['HTTP_HOST'].'/public/upload/logo/');//http图片原图访问地址
+define('UP_PATH', WWWROOTPATH.'/public/upload/logo/');//图片原图上传路径
+
+// 临时文件上传路径
+define('TF_PATH', FCPATH.'public/upload/temp/');
+define('TF_URL', 'http://'.$_SERVER['HTTP_HOST'].'/public/upload/temp/');
+// 公共目录
+define('PUBLIC_PATH', FCPATH.'public/');
+
+// 图片规格
+define("PICINDEX_S",'156_163');//首页小图片
+define("PICINDEX_B",'217_163');//首页大图片
+define("PICGOOD_S",'46_46');//商品详情小图片
+define("PICGOOD_B",'318_318');//商品详情大图片
+define("PIC436X436",'436X436');// 商品相册缩略图
+define("PIC211X211",'211X211');// 商品相册缩略图
+
+define("SALT_LEFT", md5('cmiles'));
+define('SALT_RIGHT', md5('continental'));
